4abd074151ac84ea547c6926c85f8f575eab9fd0
Valentin Stamate <stamatevalentin125@gmail.com>
2024-04-25 22:53:10 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,25 +22,10 @@ sudo apt-get install -y git
 git config --global credential.helper store
 git config --global init.defaultBranch main
 
-# Docker
-echo -e "\n${GREEN}Installing Docker...${WHITE}"
-sudo apt update -y
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-sudo rm /usr/share/keyrings/docker-archive-keyring.gpg
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update -y
-sudo apt install -y docker-ce
-sudo usermod -aG docker ${USER}
-
 # Node 20LTS
 echo -e "\n${GREEN}Installing node 20 lts...${WHITE}"
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt-get install nodejs
-
-# Angular
-echo -e "\n${GREEN}Installing Angular...${WHITE}"
-npm install -g @angular/cli
 
 # Fix time problem on dual boot
 echo -e "\n${GREEN}Fixing bual boot clock issue...${WHITE}"
